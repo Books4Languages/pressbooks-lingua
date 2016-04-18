5d9e72eee34c3b27d53b04073be49a322dcee73e
Kiur <monica.gondolini@gmail.com>
2016-04-18 12:36:49 +0200

    Revert "first commit"
    This reverts commit 1f1263c4e6a6f43ab19ea5b78615a0e8423f9cab.

--- pressbook-lingua/public/js/pressbooks-metadata-public.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function ($) {
-    'use strict';
-    $("#lb_discussion_url").after($("#lb_time_required"));
-    $("#lb_time_required").after($('#lb_custom_input1'));       
-    $('#lb_custom_input1').after($('#lb_custom_input2'));
-});
