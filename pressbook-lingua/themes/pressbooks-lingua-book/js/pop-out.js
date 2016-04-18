9c2457e61fe13b7a87db3d67b0552f68bdf04f03
Kiur <monica.gondolini@gmail.com>
2016-04-18 15:19:24 +0200

    version 0.2

---
+++ pressbook-lingua/themes/pressbooks-lingua-book/js/pop-out.js
@@ -0,0 +1,36 @@
+jQuery(document).ready(function(){
+
+		
+	jQuery('#toc').hide();
+	jQuery(document).on('click', '.toc-btn a, #toc a.close', function() {
+		if(jQuery('#toc').css('display') == 'none') {
+			jQuery('.toc-btn a').addClass('tabbed');
+			jQuery('.toc-btn').addClass('bg-color');
+			jQuery('#toc').toggle({ duration:200 });
+			return false;
+		} else {
+			jQuery('.toc-btn a').removeClass('tabbed');
+			jQuery('.toc-btn').removeClass('bg-color');
+			jQuery('#toc').toggle({ duration:100 });
+			return false;
+		}
+	});
+
+	jQuery('#page-info').hide();
+	jQuery(document).on('click', '.page-info-btn a, #page-info a.close', function(){
+		if(jQuery('#page-info').css('display') == 'none'){
+			jQuery('.page-info-btn a').addClass('tabbed');
+			jQuery('.page-info-btn').addClass('bg-color');
+			jQuery('#page-info').toggle({ duration:200 });
+			return false;
+		}else{ 
+			jQuery('.page-info-btn a').removeClass('tabbed');
+			jQuery('.page-info-btn').removeClass('bg-color');
+			jQuery('#page-info').toggle({ duration:100 });
+			return false;
+		}
+	});
+
+});
+
+
