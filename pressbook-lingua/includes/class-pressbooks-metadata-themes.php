911c132677fffd4e6ae28791180069dad8f4ab1b
Kiur <monica.gondolini@gmail.com>
2016-04-21 16:14:41 +0200

    style changes

--- pressbook-lingua/includes/class-pressbooks-metadata-themes.php
+++ pressbook-lingua/includes/class-pressbooks-metadata-themes.php
@@ -63,6 +63,7 @@ class Pressbooks_Metadata_Themes {
 	public function enqueue_custom_themes() {
 
 		wp_register_style( 'pressbooks-lingua-book', plugin_dir_url( dirname( __FILE__ ) ) . 'themes/pressbooks-lingua-book/style.css', array( 'pressbooks-book' ), $this->version, 'all' );
+		wp_register_style( 'pressbooks-lingua-book', plugin_dir_url( dirname( __FILE__ ) ) . 'themes/pressbooks-lingua-book/style.scss', array( 'pressbooks-book' ), $this->version, 'all' );
 
 		wp_enqueue_style( 'pressbooks-book' );
 		wp_enqueue_style( 'pressbooks-lingua-book' );
