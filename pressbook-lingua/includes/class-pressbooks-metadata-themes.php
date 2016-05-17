8ca7169d813ac2efef4218726acf5b991d8ac08d
Kiur <monica.gondolini@gmail.com>
2016-05-17 14:50:29 +0200

    themes upload

--- pressbook-lingua/includes/class-pressbooks-metadata-themes.php
+++ pressbook-lingua/includes/class-pressbooks-metadata-themes.php
@@ -64,12 +64,9 @@ class Pressbooks_Metadata_Themes {
 
 		wp_register_style( 'pressbooks-lingua-book', plugin_dir_url( dirname( __FILE__ ) ) . 'themes/pressbooks-lingua-book/style.css', array( 'pressbooks-book' ), $this->version, 'all' );
 		wp_register_style( 'pressbooks-lingua-book', plugin_dir_url( dirname( __FILE__ ) ) . 'themes/pressbooks-lingua-book/style.scss', array( 'pressbooks-book' ), $this->version, 'all' );
-		wp_register_style( 'pressbooks-lingua-catalog', plugin_dir_url( dirname(__FILE__)) . 'themes/pressbooks-lingua-catalog/style.css', array( 'pressbooks-book' ), $this->version, 'all' );
-
-
+		
 		wp_enqueue_style( 'pressbooks-book' );
 		wp_enqueue_style( 'pressbooks-lingua-book' );
-		wp_enqueue_style( 'pressbooks-lingua-catalog');
 
 	}
         
